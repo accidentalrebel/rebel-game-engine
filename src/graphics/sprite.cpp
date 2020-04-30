73e8f10a799bbab54347b1bfc1add4ae5306c2f4
ARebel <accidentalrebel@gmail.com>
2020-05-01 07:45:02 +0800

    Now using orthographic camera.

--- src/graphics/sprite.cpp
+++ src/graphics/sprite.cpp
@@ -44,13 +44,15 @@ void Sprite::draw()
 {
  	shader->use();
 	shader->setInt("texture1", 0);
-
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+	
+	// glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+	glm::mat4 projection = glm::ortho(-400.0f, 400.0f, -300.0f, 300.0f, 0.1f, 100.0f);
+	
 	glm::mat4 view = glm::mat4(1.0f);
 	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
 	
 	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+	model = glm::scale(model, glm::vec3(400.0f));
 
 	shader->setMat4("projection", projection);
 	shader->setMat4("view", view);
