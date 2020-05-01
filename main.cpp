9b3d79287b2a6339ce41a84bc5cf327693db0e9c
ARebel <accidentalrebel@gmail.com>
2020-05-01 11:03:02 +0800

    Can now move a sprite using the keyboard.

--- main.cpp
+++ main.cpp
@@ -19,24 +19,34 @@ int main()
 	// NOTE: We may have no need for an initializer.
 	sprite->initialize(shader, "assets/textures", "tile.png");
 
+	glm::vec2 pinkSquarePosition(400.0, 300.0f);
+
 	while(!window->canClose())
 	{
 		g_engine->processInput();
 		window->clear();
 
-		// TODO: Think of component system? To add a Transform component? Or make it simple?
-		// TODO: Draw should accept a color.
-		// TODO: Width and height should be saved in Window class.
-
+		// TODO: Window width and height should be saved in Window class.
 		// NOTE: Consider having a renderer class where you call draw.
-		// Or maybe change window to renderer?
+		// Or maybe change window to renderer? 
+		
 		sprite->draw(glm::vec2(0.0f, 0.0f), 100, 100);
 		sprite->draw(glm::vec2(100.0f, 0.0f), 100, 100);
 		sprite->draw(glm::vec2(800.0f, 0.0f), 100, 100);
 		sprite->draw(glm::vec2(800.0f, 600.0f), 100, 100);
 		sprite->draw(glm::vec2(0.0f, 600.0f), 50, 50);
 		sprite->draw(glm::vec2(400.0f, 300.0f), 100, 100);
-		sprite->draw(glm::vec2(400.0f, 300.0f), 50, 50, glm::vec3(1.0f, 0.0f, 1.0f));
+		sprite->draw(pinkSquarePosition, 50, 50, glm::vec3(1.0f, 0.0f, 1.0f));
+
+		// TODO: Make a simpler to use Input manager. Like Unity's "Input.GetKey"
+		if ( glfwGetKey(window->glWindow, GLFW_KEY_COMMA) == GLFW_PRESS)
+			pinkSquarePosition.y += 1;
+		if ( glfwGetKey(window->glWindow, GLFW_KEY_O) == GLFW_PRESS)
+			pinkSquarePosition.y -= 1;
+		if ( glfwGetKey(window->glWindow, GLFW_KEY_A) == GLFW_PRESS)
+			pinkSquarePosition.x -= 1;
+		if ( glfwGetKey(window->glWindow, GLFW_KEY_E) == GLFW_PRESS)
+			pinkSquarePosition.x += 1;
 
 		window->swap();
 	}
