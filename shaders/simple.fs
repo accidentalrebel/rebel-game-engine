d7928cad18d1488199e99c84bafe41f6dd580050
ARebel <accidentalrebel@gmail.com>
2020-05-24 10:08:59 +0000

    Fixed bug with lighting.

--- shaders/simple.fs
+++ shaders/simple.fs
@@ -27,18 +27,18 @@ uniform vec3 viewPos;
 
 void main()
 {
-	vec3 ambient = directionLight.ambient * material.ambient;
+	vec3 ambient = material.ambient; // * directionLight.ambient;
 
 	vec3 normals = normalize(Normals);
 	vec3 lightDir = normalize(-directionLight.direction - FragPos);
 	float diff = max(dot(normals, lightDir), 0.0);
-	vec3 diffuse = directionLight.diffuse * (diff * material.diffuse);
+	vec3 diffuse = (diff * material.diffuse) * directionLight.diffuse;
 
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, normals);
 	float spec  = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 	vec3 specular = directionLight.specular * (spec * material.specular);;
 	
-	vec3 result = (specular + ambient + diffuse);
+	vec3 result = ambient + diffuse + specular;
 	FragColor = texture(texture1, TexCoords) * vec4(result, 1.0);
 }
