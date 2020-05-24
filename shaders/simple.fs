2f6f6b708718dce77c0a4a517f7089f66c7ab460
ARebel <accidentalrebel@gmail.com>
2020-05-24 09:32:05 +0000

    Shader now has DirectionLight struct.

--- shaders/simple.fs
+++ shaders/simple.fs
@@ -7,30 +7,37 @@ struct Material
 	float shininess;
 };
 
+struct DirectionLight {
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 out vec4 FragColor;
 in vec3 FragPos;
 in vec3 Normals;
 in vec2 TexCoords;
 
+uniform DirectionLight directionLight;
 uniform Material material;
 uniform sampler2D texture1;
 uniform vec3 viewPos;
-uniform vec3 lightColor;
-uniform vec3 lightDirection;
 
 void main()
 {
-	vec3 ambient = lightColor * material.ambient;
+	vec3 ambient = directionLight.ambient * material.ambient;
 
 	vec3 normals = normalize(Normals);
-	vec3 lightDir = normalize(-lightDirection - FragPos);
+	vec3 lightDir = normalize(-directionLight.direction - FragPos);
 	float diff = max(dot(normals, lightDir), 0.0);
-	vec3 diffuse = lightColor * (diff * material.diffuse);
+	vec3 diffuse = directionLight.diffuse * (diff * material.diffuse);
 
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, normals);
 	float spec  = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = lightColor * (spec * material.specular);;
+	vec3 specular = directionLight.specular * (spec * material.specular);;
 	
 	vec3 result = (specular + ambient + diffuse);
 	FragColor = texture(texture1, TexCoords) * vec4(result, 1.0);
