67b4b83560580dca6e784c65d17c997fbc7a2cc3
arebel@win <accidentalrebel@gmail.com>
2020-05-26 18:32:17 +0800

    Refactored the fragment shader.

--- shaders/simple.fs
+++ shaders/simple.fs
@@ -23,20 +23,27 @@ uniform DirectionLight directionLight;
 uniform Material material;
 uniform vec3 viewPos;
 
+vec3 CalculateDirectionalLight(DirectionLight light, vec3 normal, vec3 viewDir);
+
 void main()
 {
-	vec3 ambient = directionLight.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
-
-	vec3 normals = normalize(Normals);
-	vec3 lightDir = normalize(-directionLight.direction - FragPos);
-	float diff = max(dot(normals, lightDir), 0.0);
-	vec3 diffuse = diff * directionLight.diffuse * vec3(texture(material.texture_diffuse1, TexCoords));
-
+	vec3 normal = normalize(Normals);
 	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, normals);
-	float spec  = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = spec * directionLight.specular * vec3(texture(material.texture_specular1, TexCoords));
-	
-	vec3 result = ambient + diffuse + specular;
+	vec3 result = CalculateDirectionalLight(directionLight, normal, viewDir);
 	FragColor = vec4(result, 1.0);
+}
+
+vec3 CalculateDirectionalLight(DirectionLight light, vec3 normal, vec3 viewDir)
+{
+	vec3 lightDir = normalize(-light.direction - FragPos);
+	float diff = max(dot(normal, lightDir), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec  = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+	vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+	vec3 diffuse = diff * light.diffuse * vec3(texture(material.texture_diffuse1, TexCoords));
+	vec3 specular = spec * light.specular * vec3(texture(material.texture_specular1, TexCoords));
+	
+	return (ambient + diffuse + specular);
 }
