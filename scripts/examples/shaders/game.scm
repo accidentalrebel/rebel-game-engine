a099a0bc9e39d3e5d2abc74c4b49d92db34a486e
ARebel <accidentalrebel@gmail.com>
2020-06-19 21:53:22 +0800

    Can now spawn a light cube.

--- scripts/examples/shaders/game.scm
+++ scripts/examples/shaders/game.scm
@@ -1,7 +1,8 @@
 (include-relative "../../extensions/fpcam")
 
-(define *box*)
+(define *cube*)
 (define *box-shader*)
+(define *light-shader*)
 
 (define *point-lights* '())
 
@@ -14,13 +15,15 @@
   
   ;; Generate a cube mesh and load it as a model
   ;; TODO; Fix the mesh normals
-  (set! *box* (model:load_from_mesh (mesh:generate_cube 1.0 1.0 1.0)))
+  (set! *cube* (model:load_from_mesh (mesh:generate_cube 1.0 1.0 1.0)))
+  (set! *light-cube* (model:load_from_mesh (mesh:generate_cube 0.5 0.5 0.5)))
 
   ;; Load the texture and assign is as a texture diffuse
-  (model:texture_diffuse! *box* (texture:load "assets/textures/texel-checker.png"))
+  (model:texture_diffuse! *cube* (texture:load "assets/textures/texel-checker.png"))
 
   ;; TODO; Make own shader for this example only
   (set! *box-shader* (shader:create "shaders/simple-3d.vs" "shaders/simple.fs"))
+  (set! *light-shader* (shader:create "shaders/light-shader.vs" "shaders/light-shader.fs"))
   )
 
 (define (update)
@@ -33,7 +36,13 @@
 
   (shader:use *box-shader*)
 
-  (model:draw *box* '(0.0 0.0 0.0))
+  (model:draw *cube* '(0.0 0.0 0.0))
+
+  (shader:use *light-shader*)
+
+  (model:draw *light-cube*
+	      (light:point:position
+	       (first *point-lights*)))
 
   (window:swap)
   )
