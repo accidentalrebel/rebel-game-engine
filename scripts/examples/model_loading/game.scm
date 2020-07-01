909ba5b1022de59905bc2f344923a599acc28014
ARebel <accidentalrebel@gmail.com>
2020-07-01 19:14:56 +0800

    Can now load shaders.

--- scripts/examples/model_loading/game.scm
+++ scripts/examples/model_loading/game.scm
@@ -1,15 +1,28 @@
 (define *basic-shader*)
+(define *backpack*)
 
 ;; Setup the first person camera extension
 (include-relative "../../extensions/fpcam")
 
 (define (init)
-  (set! *basic-shader* (shader:create "shaders/basic.vs" "shaders/basic.fs")))
+  ;; Load the model object
+  ;; model:load also loads the textures automatically
+  (set! *backpack* (model:load "assets/models/backpack/backpack.obj"))
+
+  ;; Load a basic shader
+  (set! *basic-shader* (shader:create "shaders/model-loading.vs" "shaders/model-loading.fs")))
 
 (define (update)
+  ;; This is needed for the first person camera extension to work
   (fpcam:update))
 
 (define (render)
   (window:clear '(0.1 0.1 0.1))
-  (window:swap)
-  )
+
+  ;; Use the basic-shader
+  (shader:use *basic-shader*)
+
+  ;; Draw the backpack model
+  (model:draw *backpack* '(0 0 0) '(1 1 1))
+  
+  (window:swap))
--- shaders/model-loading.fs
+++ shaders/model-loading.fs
@@ -3,9 +3,15 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D texture_diffuse1;
+struct Material
+{
+	sampler2D texture_diffuse1;
+	sampler2D texture_specular1;
+};
+
+uniform Material material;
 
 void main()
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+	FragColor = texture(material.texture_diffuse1, TexCoords);
 }
