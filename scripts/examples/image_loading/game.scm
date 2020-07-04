0189de6bbe155d79d0c6b95f2532c1756232cbe0
ARebel <accidentalrebel@gmail.com>
2020-07-04 13:17:42 +0800

    Removed the debug displays.

--- scripts/examples/image_loading/game.scm
+++ scripts/examples/image_loading/game.scm
@@ -1,25 +1,18 @@
 (define *sprite*)
 (define *model-shader*)
 
-(display ">> AYO")
-
 ;; Setup the first person camera extension
 (include-relative "../../extensions/fpcam")
 
-(display ">> YO")
-
 (define (init)
-  (display ">> 1")
   (set! *sprite*
 	(model:load_from_mesh (mesh:generate_cube 1.0 1.0 1.0)))
 
-  (display ">> 2")
   ;; TODO; Not specifying the third parameter for material:load_texture should default to "texture_diffuse"
   ;; TODO; Consider changing the third parameter to an Enum
   (model:add_texture *sprite*
   		     (material:load_texture "assets/textures/" "awesomeface.png" "texture_diffuse"))
 
-  (display ">> 3")
   (set! *model-shader*
 	(shader:create "shaders/simple-3d.vs" "shaders/simple.fs"))
   	;; (shader:create "shaders/model-loading.vs" "shaders/model-loading.fs"))
