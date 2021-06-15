dd9787b9bcb10066bf849866cd185058469ab239
accidentalrebel <accidentalrebel@gmail.com>
2021-06-15 12:18:51 +0800

    Can now determine ID, width, and height.

--- src/graphics/text.c
+++ src/graphics/text.c
@@ -33,30 +33,37 @@ void TextLoadFont(Text* text, Font *font) {
 	printf("########### %s\n", charsObj->name->string);
 	assert(strcmp(charsObj->name->string, "chars") == 0);
 
-	struct json_value_s* chars_array_value = charsObj->value;
-	assert(chars_array_value->type == json_type_array);
+	struct json_value_s* charsArrayValue = charsObj->value;
+	assert(charsArrayValue->type == json_type_array);
 
-	struct json_array_s* charsArray = (struct json_array_s*)chars_array_value->payload;
+	struct json_array_s* charsArrayPayload = (struct json_array_s*)charsArrayValue->payload;
 
-	struct json_array_element_s* charObj = charsArray->start;
+	struct json_array_element_s* charObj = charsArrayPayload->start;
 	assert(charObj->value->type == json_type_object);
 
-	struct json_object_s* charObjE = charObj->value->payload;
-
-	struct json_object_element_s* current = charObjE->start;
-
-	for ( int i = 0; i < (int)charsArray->length ; i++ )
+	for ( int i = 0; i < (int)charsArrayPayload->length ; i++ )
 	{
+		struct json_object_s* charObjE = charObj->value->payload;
+		struct json_object_element_s* current = charObjE->start;
+		
 		while ( current != NULL )
 		{
 			printf("########### %s\n", current->name->string);
 			if ( strcmp(current->name->string, "id") == 0 )
-			{
-				printf("########### FOUND ######## %s\n", ((struct json_string_s*)current->value->payload)->string);
-			}
+				printf("########### ID ######## %s\n", ((struct json_string_s*)current->value->payload)->string);
+			else if ( strcmp(current->name->string, "width") == 0 )
+				printf("########### WIDTH ######## %s\n", ((struct json_string_s*)current->value->payload)->string);
+			else if ( strcmp(current->name->string, "height") == 0 )
+				printf("########### HEIGHT ######## %s\n", ((struct json_string_s*)current->value->payload)->string);
 		
 			current = current->next;
 		}
+
+		charObj = charObj->next;
+		if ( charObj == NULL )
+			break;
+
+		assert(charObj->value->type == json_type_object);
 	}
 	
 	free(root);
