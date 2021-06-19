1e65734b1412ef877da2f6f97d36eef441d3ed5f
accidentalrebel <accidentalrebel@gmail.com>
2021-06-19 09:58:51 +0800

    Can skip the building stage.

--- scripts/tools/rge.el
+++ scripts/tools/rge.el
@@ -2,20 +2,23 @@
 
 (setq last-rge-script-directory nil)
 
-(defun build-and-run-example ()
-  "Find build.sh and run.sh from within the project and execute it."
-  (interactive)
-  (message "starting...")
-  (let ((script-dir (if (string= (file-name-extension buffer-file-name)
+(defun build-and-run-example (arg)
+  "Find build.sh and run.sh from within the project and execute it.
+Call with universar ARG (Ctrl+U) to skip building stage."
+  (interactive "P")
+  (let ((can-only-run (equal current-prefix-arg '(4)))
+	(script-dir (if (string= (file-name-extension buffer-file-name)
 				 "scm")
 			(progn
 			  (setq last-rge-script-directory (file-name-directory buffer-file-name))
 			  last-rge-script-directory)
 		      last-rge-script-directory))
 	(makefile-dir (locate-dominating-file default-directory "Makefile")))
-    (compile (concat "cd "
-		     makefile-dir
-		     " && make build && cd "
+    (compile (concat (when (not can-only-run)
+		       (concat "cd "
+			       makefile-dir
+			       " && make build && "))
+		     "cd "
 		     script-dir
 		     " && make -C "
 		     makefile-dir
